c7e91753db937dd3029232b411045d528651fd65
MishaKarliychuk <mishakarliyshuk@gmail.com>
2022-04-18 21:09:44 +0300

    Бесконечный цикл

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ url_searching = "https://play.google.com/store/search?q={}&c=apps"
 COUNT_OF_MAX_MISTAKE = 0
 COUNT_OF_MISTAKE = 0
 MISTAKEN_APP_URL = []
-PAGINATION = 1
+PAGINATION = 200
 
 
 ARABIC_WORDS = ["شأس" , "التبتل" , "رمضان" , "واهية",  "قرابين" , "اللبن" , "استشراف" , "رفث",  "صوم" , "متضائل",  "ثيب" , "أثر",  "الورى",  "تبتل" , "عن" , "الزواج" , "المعجم" , "قيان" , "استقل" ,  "عضم", "علق"]
--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import time
 from services import *
-# import asyncio
+import asyncio
 import aiohttp
 import csv
 
@@ -18,45 +18,48 @@ app_urls = []
 emails = []
 proxy_list = str(read_proxy_file()).split('\n')
 
-for proxy in proxy_list:
-    main_get_app_urls(app_urls, proxy)
+while True:
+    for proxy in proxy_list:
+        main_get_app_urls(app_urls, proxy)
 
-app_urls = list(set(app_urls))
+    app_urls = list(set(app_urls))
 
-async def get_html_of_app_page(url):
-    global COUNT_OF_MAX_MISTAKE, COUNT_OF_MISTAKE
-    try:
-        async with aiohttp.request('GET', url) as res:
-            text = await res.text()
-            soup = BeautifulSoup(text, 'html.parser')
-            email = get_email_of_app(soup)
-            print(email)
-            emails.append(email)
-    except aiohttp.client_exceptions.ServerDisconnectedError:
-        time.sleep(3)
-    except aiohttp.client_exceptions.ClientOSError:
-        print("Disconnect server. COUNT_OF_MISTAKE: ", COUNT_OF_MAX_MISTAKE)
-        MISTAKEN_APP_URL.append(url)
+    async def get_html_of_app_page(url):
+        global COUNT_OF_MAX_MISTAKE, COUNT_OF_MISTAKE
+        try:
+            async with aiohttp.request('GET', url) as res:
+                text = await res.text()
+                soup = BeautifulSoup(text, 'html.parser')
+                email = get_email_of_app(soup)
+                print(email)
+                emails.append(email)
+        except aiohttp.client_exceptions.ServerDisconnectedError:
+            time.sleep(3)
+        except aiohttp.client_exceptions.ClientOSError:
+            print("Disconnect server. COUNT_OF_MISTAKE: ", COUNT_OF_MAX_MISTAKE)
+            MISTAKEN_APP_URL.append(url)
 
-event_loop = asyncio.new_event_loop()
-asyncio.set_event_loop(event_loop)
-async def main():
-    for page in range(round(len(app_urls) / PAGINATION)):
-        tasks = []
-        from_ = page * PAGINATION
-        to_ = (page+1) * PAGINATION
-        for app_url in list(app_urls)[from_:to_:]:
-            tasks.append(asyncio.ensure_future(get_html_of_app_page(app_url)))
-        await asyncio.gather(*tasks)
-        time.sleep(25)
-event_loop.run_until_complete(main())
+    event_loop = asyncio.new_event_loop()
+    asyncio.set_event_loop(event_loop)
+    async def main():
+        for page in range(round(len(app_urls) / PAGINATION)):
+            tasks = []
+            from_ = page * PAGINATION
+            to_ = (page+1) * PAGINATION
+            for app_url in list(app_urls)[from_:to_:]:
+                tasks.append(asyncio.ensure_future(get_html_of_app_page(app_url)))
+            await asyncio.gather(*tasks)
+            time.sleep(25)
+    event_loop.run_until_complete(main())
 
-print("Всего emails: ", len(set(emails)), " Всего ошибок было: ", len(MISTAKEN_APP_URL))
-emails = list(set(emails))
-with open("EMAILS.csv", "w") as file:
-    wr = csv.writer(file)
-    emails_row = [[email] for email in emails]
-    wr.writerows(emails_row)
+    print("Всего emails: ", len(set(emails)), " Всего ошибок было: ", len(MISTAKEN_APP_URL))
+    emails = list(set(emails))
+    with open("EMAILS.csv", "w") as file:
+        wr = csv.writer(file)
+        emails_row = [[email] for email in emails]
+        wr.writerows(emails_row)
+
+    time.sleep(2*60)
 #
 #         file = open("EMAILS.csv", "rb")
 #         bot.send_document(message.chat.id, file)
